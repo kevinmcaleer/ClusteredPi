720c228e37edd4668918d1d2646dbab9a5674a32
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-03 15:03:18 +0000

    added kevsrobots stack

---
+++ stacks/kevsrobots/deploy.sh
@@ -0,0 +1,6 @@
+# Build your Docker image
+docker build -t 192.168.2.1:5000/kevsrobots:latest .
+
+# Push your Docker image
+# docker push search:latest
+docker push 192.168.2.1:5000/kevsrobots:latest
