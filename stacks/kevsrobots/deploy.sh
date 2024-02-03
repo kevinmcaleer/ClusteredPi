bfb527e45c5860027aebe66d7ddceedf31edbd91
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-03 23:26:23 +0000

    updated kevsrobots deploy

--- stacks/kevsrobots/deploy.sh
+++ stacks/kevsrobots/deploy.sh
@@ -1,5 +1,5 @@
 # Build your Docker image
-docker build -t 192.168.2.1:5000/kevsrobots:latest .
+docker build -t 192.168.2.1:5000/kevsrobots:latest . --no-cache
 
 # Push your Docker image
 # docker push search:latest
