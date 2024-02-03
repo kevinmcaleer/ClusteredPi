64f7dd2e3f65fe477625ed7ab302dc1d8e2d31e7
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-03 23:27:40 +0000

    added --image to deploy

--- stacks/kevsrobots/deploy.sh
+++ stacks/kevsrobots/deploy.sh
@@ -4,3 +4,5 @@ docker build -t 192.168.2.1:5000/kevsrobots:latest . --no-cache
 # Push your Docker image
 # docker push search:latest
 docker push 192.168.2.1:5000/kevsrobots:latest
+docker stack deploy -c docker-compose.yml kevsrobots
+docker service update --image 192.168.2.1:5000/kevsrobots:latest kevsrobots
