73070e16491b0ab02eb9b14cabdaf55ab230e260
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-14 22:49:24 +0000

    updated script to get ip

--- stacks/octapi/start_node.sh
+++ stacks/octapi/start_node.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -e
 sleep 1
-_IP=$(hostname -I | awk '{print $1}')
+_IP=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+
+# _IP=$(hostname -I | awk '{print $1}')
 dispynode.py -i $_IP --daemon -p 8002
