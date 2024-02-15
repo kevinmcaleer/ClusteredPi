8d80b6f78e5848e0cb1dfa321f3f1397c0a249d8
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-15 00:20:29 +0000

    removed port

--- stacks/octapi/start_node.sh
+++ stacks/octapi/start_node.sh
@@ -4,4 +4,4 @@ sleep 1
 _IP=$(ip addr show eth0 | grep 'inet' | awk '{print $2}' | cut -d/ -f1 | head -n 1)
 
 # _IP=$(hostname -I | awk '{print $1}')
-dispynode.py -i $_IP -p 8002 --daemon --clean
+dispynode.py -i $_IP  --daemon --clean
