bb556db418b6373297b4987c36fe493e6887a9e6
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-15 11:50:22 +0000

    added debug

--- stacks/octapi/start_node.sh
+++ stacks/octapi/start_node.sh
@@ -5,4 +5,4 @@ _IP=$(ip addr show eth0 | grep 'inet' | awk '{print $2}' | cut -d/ -f1 | head -n
 
 # _IP=$(hostname -I | awk '{print $1}')
 dispynode.py -i $_IP  --daemon --clean
-# dispynode.py -i 192.168.2.2 --daemon --clean
+# dispynode.py -i 192.168.2.2 --daemon --clean --debug
