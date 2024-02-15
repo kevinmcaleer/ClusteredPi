41686a2d534f2cb0ae6bc3b16c2c8b9aa3f7bd6b
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-15 00:58:40 +0000

    updated server

--- stacks/octapi/start_node.sh
+++ stacks/octapi/start_node.sh
@@ -4,4 +4,5 @@ sleep 1
 _IP=$(ip addr show eth0 | grep 'inet' | awk '{print $2}' | cut -d/ -f1 | head -n 1)
 
 # _IP=$(hostname -I | awk '{print $1}')
-dispynode.py -i $_IP  --daemon --clean
+# dispynode.py -i $_IP  --daemon --clean
+dispynode.py -i 192.168.2.2 --daemon --clean
