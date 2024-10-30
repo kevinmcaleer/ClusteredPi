7e9f0d5d3f329fcc36a11123573bc8244f1983d0
Kevin McAleer <kevinmcaleer@gmail.com>
2024-10-30 21:00:00 +0000

    updated script

--- stacks/cratedb/crate_hosts.sh
+++ stacks/cratedb/crate_hosts.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 # Get the hostname of the machine to use as CRATE_NODE_NAME
-CRATE_NODE_NAME=$(hostname)
+export CRATE_NODE_NAME=$(hostname)
 
 # Define CRATE_DISCOVERY_SEED_HOSTS with specific IPs
-CRATE_DISCOVERY_SEED_HOSTS="192.168.1.10:4300,192.168.2.1:4300"
-
-# Write environment variables to .env file for Docker Compose
-echo "CRATE_NODE_NAME=$CRATE_NODE_NAME" > .env
-echo "CRATE_DISCOVERY_SEED_HOSTS=$CRATE_DISCOVERY_SEED_HOSTS" >> .env
+export CRATE_DISCOVERY_SEED_HOSTS="192.168.1.10:4300,192.168.2.1:4300"
 
 echo "Starting CrateDB with node name: $CRATE_NODE_NAME"
 echo "Discovery seed hosts: $CRATE_DISCOVERY_SEED_HOSTS"
